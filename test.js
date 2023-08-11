e8517af12a6e7af663d7ace472bcf9cc45a3823d
Ravish Arya <ravish.arya@trigyn.com>
2023-08-11 18:40:25 +0530

    test file fixed

--- test.js
+++ test.js
@@ -1,18 +1,26 @@
 const { queryFBMetrics, queryFBMetricsbyDateRange } = require("./query");
 
-async function test(fromDate, toDate) {
+async function test() {
     let result = null;
+    const fromDate = process.argv[2];
+    const toDate = process.argv[3];
+
+    console.log('fromdate=', fromDate);
+    console.log('todate=', toDate);
 
     if (fromDate && toDate) {
         result = await queryFBMetricsbyDateRange(fromDate, toDate);
     } else if (fromDate) {
         result = await queryFBMetrics(fromDate);
     } 
-    // else {
-    //     result = await queryFBMetrics('20230802');   
-    // }
+    else {
+        result = {
+            status: 'error',
+            body: "Invalid request. Incorrect parameters passed.",
+        }
+    }
     console.log(result);
     return result;
 }
 
-test(fromDate=null, toDate=null);
+test();
